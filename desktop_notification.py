bc0dc1af0f977541d05256dbbb14addff53de0ee
Hetvi Shah <98590708+hetvishah09@users.noreply.github.com>
2022-07-30 15:51:20 +0530

    Add files via upload

---
+++ desktop_notification.py
@@ -0,0 +1,13 @@
+import time
+
+from plyer import notification
+
+if __name__=="__main__":
+
+		notification.notify(
+		title="** Please drink water now!! **",
+		message = "its good for body!",
+		# app_icon = "E:\python_\PROJECTS\icon.ico",
+		timeout=5
+	)
+	
